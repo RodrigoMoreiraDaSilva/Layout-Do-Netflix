f15ec78472c46913694d20e2d7d8b2023c68c077
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-02-23 21:01:28 -0300

    Edição do cabeçalho (finalizado) e da seção do filme-principal

---
+++ js/procedimentos.js
@@ -0,0 +1,31 @@
+function sininhoMouseEmCima(imagem) {
+    imagem.src = "./img/Elementos/sininho-cinza.png";
+}
+
+function sininhoMouseFora(imagem) {
+    imagem.src = "./img/Elementos/sininho-branco.png";
+}
+
+function lupaEmCima(imagem) {
+    imagem.src = "./img/Elementos/busca-cinza.png";
+}
+
+function lupaFora(imagem) {
+    imagem.src = "./img/Elementos/busca-branca.png";
+}
+
+function presenteEmCima(imagem){
+    imagem.src = "./img/Elementos/presente-cinza.png";
+}
+
+function presenteFora(imagem) {
+    imagem.src = "./img/Elementos/presente-branco.png";
+}
+
+function setaMouseEmCima(imagem) {
+    imagem.src = "./img/Elementos/seta-cinza.png"
+}
+
+function setaMouseFora(imagem) {
+    imagem.src = "./img/Elementos/seta-branco.png"
+}
