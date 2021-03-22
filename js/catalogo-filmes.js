ce15f4e3f28d3bcf402163c818aad81b03cdaa6b
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-03-22 02:13:43 -0300

    Novo carrossel com animação

---
+++ js/catalogo-filmes.js
@@ -0,0 +1,10 @@
+var secaoTitulo = document.querySelector('.titulo-catalogo');
+var verTudo = document.querySelector('.ver-tudo');
+
+secaoTitulo.onmouseover = () => {
+    verTudo.classList.toggle('exibir-verTudo');
+}
+
+secaoTitulo.onmouseout = () => {
+    verTudo.classList.toggle('exibir-verTudo');
+}
--- js/setup.js
+++ js/setup.js
@@ -14,3 +14,28 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const swiper = new Swiper('.swiper-container', {
+
+    // Optional parameters
+    spaceBetween: 5,
+    slidesPerView: 2,
+    loop: true,
+    freeMode: true, 
+    loopAdditionalSlides: 5,
+    speed: 500,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        /* resolução de 640px */
+        640: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+            freeMode: false
+        }
+    }
+  });
