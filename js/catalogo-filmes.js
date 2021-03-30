d6289960d633efc943324bf0f85579bdd48d3c5b
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-03-30 11:32:04 -0300

    básico pronto

--- js/catalogo-filmes.js
+++ js/catalogo-filmes.js
@@ -1,10 +1,10 @@
-var secaoTitulo = document.querySelector('.titulo-catalogo');
-var verTudo = document.querySelector('.ver-tudo');
+// var secaoTitulo = document.querySelector('.titulo-catalogo');
+// var verTudo = document.querySelector('.ver-tudo');
 
-secaoTitulo.onmouseover = () => {
-    verTudo.classList.toggle('exibir-verTudo');
-}
+// secaoTitulo.onmouseover = () => {
+//     verTudo.classList.toggle('exibir-verTudo');
+// }
 
-secaoTitulo.onmouseout = () => {
-    verTudo.classList.toggle('exibir-verTudo');
-}
+// secaoTitulo.onmouseout = () => {
+//     verTudo.classList.toggle('exibir-verTudo');
+// }
