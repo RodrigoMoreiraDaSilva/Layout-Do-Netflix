cf4cbfa82445aa6377bb4c86408a927b6509d461
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-04-20 13:00:25 -0300

    1º carrossel terminado

--- js/setup.js
+++ js/setup.js
@@ -1,41 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
-
 const swiper = new Swiper('.swiper-container', {
-
     // Optional parameters
     spaceBetween: 5,
     slidesPerView: 2,
     loop: true,
-    freeMode: true, 
+    freeMode: true,
     loopAdditionalSlides: 5,
-    speed: 500,
+    speed: 600,
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
     breakpoints: {
-        /* resolução de 640px */
+        //quando o tamanho da janela estiver >= 640px
+
         640: {
             slidesPerView: 5,
             slidesPerGroup: 5,
-            freeMode: false
+            freeMode: false,
         }
     }
   });
